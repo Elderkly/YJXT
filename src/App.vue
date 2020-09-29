e8558aed6becf58b0fee04ad797c87bbecc4bc20
Elderkly <897676943@qq.com>
2020-09-29 12:00:48 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,23 @@
         name: 'App',
         components: {
             Header
+        },
+        mounted() {
+            const data = {
+                routename: 'yunnan-101',
+                start_time: '2020-09-27',
+                end_time: '2020-09-27',
+                datatype: '预警次数',
+                ratio: 10
+            }
+            fetch('http://134.175.65.106/statistics/statistics1.0.php?',{
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                body: JSON.stringify(data), // must match 'Content-Type' header
+            })
+            .then(res => res.json())
+            .then(res => {
+                console.log('123123',res)
+            })
         }
     }
 </script>
